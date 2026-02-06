199c2967ce2881c7fab58cb9d697b5b1cdbe2a0a
Dmitrii Anisimov <kokopokoooooo@gmail.com>
2026-02-06 16:03:47 +0300

    start generate pics deploy 2.0

---
+++ telegram-bot-microservices/alembic/versions/20260206_add_image_generation_tracking.py
@@ -0,0 +1,32 @@
+"""Add image generation tracking to dialogs
+
+Revision ID: 020
+Revises: 019
+Create Date: 2026-02-06
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+revision = '020'
+down_revision = '019'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    """Add last_image_generation_at column to dialogs table."""
+    op.add_column(
+        'dialogs',
+        sa.Column(
+            'last_image_generation_at',
+            sa.Integer(),
+            nullable=True,
+            comment='Message count when last image was generated (for trigger frequency tracking)'
+        )
+    )
+
+
+def downgrade() -> None:
+    """Remove last_image_generation_at column from dialogs table."""
+    op.drop_column('dialogs', 'last_image_generation_at')
